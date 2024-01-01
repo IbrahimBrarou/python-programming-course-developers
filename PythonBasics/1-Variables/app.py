5455671c97a85bf2e34152207ef8ee220e1ef33b
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2024-01-01 23:10:13 +0100

    initial commit

---
+++ PythonBasics/1-Variables/app.py
@@ -0,0 +1,12 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "python"
+
+
+x = 1
+y = 2
+x, y = 1, 2
+
+
+x = y = 1
---
+++ PythonBasics/2-DynamicTyping/app.py
@@ -0,0 +1,4 @@
+# c, c+, Java are static which means it is needed to declare a type for variables. type of variables is deterrmened at compile time
+# Ruby, Python are dynamic. type of variable is determined at runtime
+students_count = 1000
+print(type(students_count))
